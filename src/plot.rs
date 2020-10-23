31876a991ecce26735434060c0781cf2ad354423
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-10-23 02:19:11 +0200

    Convert spaces to tabs

--- src/plot.rs
+++ src/plot.rs
@@ -2,40 +2,40 @@ use {::xy::{xy,vec2}, image::{Image, bgrf, bgra8}};
 
 fn line(target: &mut Image<&mut[bgra8]>, p0: vec2, p1: vec2, color: bgrf) {
 	use num::{abs, fract};
-  let d = p1 - p0;
-  let (transpose, p0, p1, d) = if abs(d.x) < abs(d.y) { (true, p0.yx(), p1.yx(), d.yx()) } else { (false, p0, p1, d) };
-  if d.x == 0. { return; } // p0==p1
-  let (p0, p1) = if p0.x > p1.x { (p1, p0) } else { (p0, p1) };
-  let gradient = d.y / d.x;
-  fn blend(target: &mut Image<&mut[bgra8]>, x: u32, y: u32, color: bgrf, coverage: f32, transpose: bool) {
+	let d = p1 - p0;
+	let (transpose, p0, p1, d) = if abs(d.x) < abs(d.y) { (true, p0.yx(), p1.yx(), d.yx()) } else { (false, p0, p1, d) };
+	if d.x == 0. { return; } // p0==p1
+	let (p0, p1) = if p0.x > p1.x { (p1, p0) } else { (p0, p1) };
+	let gradient = d.y / d.x;
+	fn blend(target: &mut Image<&mut[bgra8]>, x: u32, y: u32, color: bgrf, coverage: f32, transpose: bool) {
 		let xy{x,y} = if transpose { xy{x: y, y: x} } else { xy{x,y} };
 		if x < target.size.x && y < target.size.y { target[xy{x,y}].saturating_add_assign( (coverage*color).into() ); }
 	}
 	let (i0, intery) = {
 		let xend = f32::round(p0.x);
 		let yend = p0.y + gradient * (xend - p0.x);
-    let xgap = 1. - (p0.x + 1./2. - xend);
-    let fract_yend = yend - f32::floor(yend);
-    blend(target, xend as u32, yend as u32, color, (1.-fract_yend) * xgap, transpose);
-    blend(target, xend as u32, yend as u32+1, color, fract_yend * xgap, transpose);
-    (xend as i32, yend + gradient)
+		let xgap = 1. - (p0.x + 1./2. - xend);
+		let fract_yend = yend - f32::floor(yend);
+		blend(target, xend as u32, yend as u32, color, (1.-fract_yend) * xgap, transpose);
+		blend(target, xend as u32, yend as u32+1, color, fract_yend * xgap, transpose);
+		(xend as i32, yend + gradient)
 	};
 	let i1 = {
 		let xend = f32::round(p1.x);
-    let yend = p1.y + gradient * (xend - p1.x);
-    let xgap = p1.x + 1./2. - xend;
-    let fract_yend = yend - f32::floor(yend);
-    blend(target, xend as u32, yend as u32, color, (1.-fract_yend) * xgap, transpose);
-    blend(target, xend as u32, yend as u32+1, color, fract_yend * xgap, transpose);
-    xend as u32
+		let yend = p1.y + gradient * (xend - p1.x);
+		let xgap = p1.x + 1./2. - xend;
+		let fract_yend = yend - f32::floor(yend);
+		blend(target, xend as u32, yend as u32, color, (1.-fract_yend) * xgap, transpose);
+		blend(target, xend as u32, yend as u32+1, color, fract_yend * xgap, transpose);
+		xend as u32
 	};
 	let x = i0+1;
 	let (mut intery, mut x) = if x < 0 { (intery+(0-x as i32) as f32 * gradient, 0) } else { (intery, x as u32) };
 	while x < i1.min(if transpose { target.size.y } else { target.size.x }) {
 		blend(target, x, intery as u32, color, 1.-fract(intery), transpose);
-    blend(target, x, intery as u32+1, color, fract(intery), transpose);
-    intery += gradient;
-    x += 1;
+		blend(target, x, intery as u32+1, color, fract(intery), transpose);
+		intery += gradient;
+		x += 1;
 	}
 }
 
