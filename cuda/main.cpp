f64036f1c97b9b4f6f9694b266ff855aa037f99a
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-12-14 12:13:11 +0100

    Do not overwrite input (split output buffer)

--- cuda/main.cpp
+++ cuda/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <ios>
 #include <vector>
+#include <cmath>
+
 int main() {
 	cuInit(0);
 	CUdevice device;
@@ -27,10 +29,10 @@ int main() {
 	cuMemcpyHtoD_v2(temperature, host_temperature, len*sizeof(double));
 
 	CUdeviceptr d_temperature;
-	cuMemAlloc_v2(&temperature, len*sizeof(double));
+	cuMemAlloc_v2(&d_temperature, len*sizeof(double));
 	auto host_d_temperature = new double[len];
 	for(size_t i=0; i<len; i++) host_d_temperature[i] = NAN;
-	cuMemcpyHtoD_v2(temperature, host_temperature, len*sizeof(double));
+	cuMemcpyHtoD_v2(d_temperature, host_d_temperature, len*sizeof(double));
 
 	auto S = 9;
 
@@ -40,15 +42,21 @@ int main() {
 	for(size_t k=0; k<S-1; k++) for(size_t i=0; i<len; i++) host_amounts[k*len+i] = (double[]){0., 0., 4.874638549881687, 2.4373192749408434, 0., 0., 0., 0.}[k];
 	cuMemcpyHtoD_v2(amounts, host_amounts, (S-1)*len*sizeof(double));
 
+	CUdeviceptr d_amounts;
+	cuMemAlloc_v2(&d_amounts, (S-1)*len*sizeof(double));
+	auto host_d_amounts = new double[(S-1)*len];
+	for(size_t k=0; k<S-1; k++) for(size_t i=0; i<len; i++) host_d_amounts[k*len+i] = NAN;
+	cuMemcpyHtoD_v2(d_amounts, host_d_amounts, (S-1)*len*sizeof(double));
+
 	CUfunction function;
 	cuModuleGetFunction(&function, module,"dt");
 	auto pressure_r = 101325./8.31446261815324;
-	void* args[]{&pressure_r, &temperature, &amounts};
+	void* args[]{&len, &pressure_r, &temperature, &amounts, &d_temperature, &d_amounts};
 	cuLaunchKernel(function, len, 1, 1, stride, 1, 1, 0, stream, args, nullptr);
 	cuStreamSynchronize(stream);
-	cuMemcpyDtoH_v2(host_temperature, temperature, len*sizeof(double));
-	cuMemcpyDtoH_v2(host_amounts, amounts, (S-1)*len*sizeof(double));
-	printf("%f ", host_temperature[0]);
-	for(size_t k=0; k<S-1; k++) printf("%f ", host_amounts[k*len]);
+	cuMemcpyDtoH_v2(host_d_temperature, d_temperature, len*sizeof(double));
+	cuMemcpyDtoH_v2(host_d_amounts, d_amounts, (S-1)*len*sizeof(double));
+	printf("%f ", host_d_temperature[0]);
+	for(size_t k=0; k<S-1; k++) printf("%f ", host_d_amounts[k*len]);
 	printf("\n");
 }
