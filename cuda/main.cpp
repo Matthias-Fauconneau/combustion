552f894db3e239e4502d4b71b8f2df09959ff65c
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-12-14 12:02:24 +0100

    move CUDA include to command line

--- cuda/main.cpp
+++ cuda/main.cpp
@@ -1,4 +1,4 @@
-#include </opt/cuda/include/cuda.h>
+#include <cuda.h>
 #include <fstream>
 #include <ios>
 #include <vector>
@@ -19,17 +19,27 @@ int main() {
 	cuStreamCreateWithPriority(&stream, CU_STREAM_NON_BLOCKING, 0);
 	auto stride = 1;
 	auto len = 1*stride;
+
 	CUdeviceptr temperature;
 	cuMemAlloc_v2(&temperature, len*sizeof(double));
 	auto host_temperature = new double[len];
 	for(size_t i=0; i<len; i++) host_temperature[i] = 1000.;
 	cuMemcpyHtoD_v2(temperature, host_temperature, len*sizeof(double));
+
+	CUdeviceptr d_temperature;
+	cuMemAlloc_v2(&temperature, len*sizeof(double));
+	auto host_d_temperature = new double[len];
+	for(size_t i=0; i<len; i++) host_d_temperature[i] = NAN;
+	cuMemcpyHtoD_v2(temperature, host_temperature, len*sizeof(double));
+
 	auto S = 9;
+
 	CUdeviceptr amounts;
 	cuMemAlloc_v2(&amounts, (S-1)*len*sizeof(double));
 	auto host_amounts = new double[(S-1)*len];
 	for(size_t k=0; k<S-1; k++) for(size_t i=0; i<len; i++) host_amounts[k*len+i] = (double[]){0., 0., 4.874638549881687, 2.4373192749408434, 0., 0., 0., 0.}[k];
 	cuMemcpyHtoD_v2(amounts, host_amounts, (S-1)*len*sizeof(double));
+
 	CUfunction function;
 	cuModuleGetFunction(&function, module,"dt");
 	auto pressure_r = 101325./8.31446261815324;
