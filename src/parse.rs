5f7ae7669d3ad1088973ea45a7a20679c705e00e
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2021-02-15 16:03:00 +0100

    Typo

--- src/parse.rs
+++ src/parse.rs
@@ -4,7 +4,7 @@
 	let mut number: usize = 0;
 	let mut index = 0;
 	while index < s.len() {
-		let digit = s[index].wrapping_sub(b' ');
+		let digit = s[index] - b'0';
 		//if digit > 9 { compile_error!("Invalid number"); }
 		number += (digit as usize) * POW10[POW10.len()-s.len()+index];
 		index += 1;
